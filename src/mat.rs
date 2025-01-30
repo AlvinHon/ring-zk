0d545dc84489dade8c9504dafb17b98953208d0e
unknown <alvin.cpp@gmail.com>
2025-01-30 17:06:56 +0800

    Add rand and constructors to Mat struct

--- src/mat.rs
+++ src/mat.rs
@@ -1,14 +1,50 @@
 use std::ops::{Add, Mul, Sub};
 
-use num::{One, Zero};
+use num::{Integer, One, Zero};
 use poly_ring_xnp1::Polynomial;
+use rand::{distributions::uniform::SampleUniform, Rng};
+
+use crate::polynomial::rand_polynomial_within;
 
 /// A matrix over polynomial rings Z\[x]/(x^n+1).
+#[derive(Clone)]
 pub(crate) struct Mat<T, const N: usize> {
-    polynomials: Vec<Vec<Polynomial<T, N>>>,
+    pub(crate) polynomials: Vec<Vec<Polynomial<T, N>>>,
 }
 
 impl<T, const N: usize> Mat<T, N> {
+    /// Create a matrix (m x n) from a single polynomial.
+    pub fn from_element(m: usize, n: usize, element: Polynomial<T, N>) -> Self
+    where
+        T: Clone,
+    {
+        let polynomials = vec![vec![element.clone(); n]; m];
+        Mat { polynomials }
+    }
+
+    /// Create a matrix (m x 1) from a vector of polynomials.
+    pub fn from_vec(polynomials: Vec<Polynomial<T, N>>) -> Self {
+        Mat {
+            polynomials: polynomials.into_iter().map(|p| vec![p]).collect(),
+        }
+    }
+
+    /// Create a random matrix (m x n) with polynomials with coefficients in the
+    /// range `[-bound, bound]`.
+    pub fn rand(rng: &mut impl Rng, m: usize, n: usize, bound: T) -> Self
+    where
+        T: Integer + SampleUniform + Clone,
+    {
+        let polynomials = (0..m)
+            .map(|_| {
+                (0..n)
+                    .map(|_| rand_polynomial_within(rng, bound.clone()))
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+        Mat { polynomials }
+    }
+
     pub fn dim(&self) -> (usize, usize) {
         let m = self.polynomials.len();
         let n = if self.polynomials.is_empty() {
@@ -64,7 +100,7 @@ impl<T, const N: usize> Mat<T, N> {
     /// Extend the matrix by adding rows.
     /// Original dimensions: m x n;
     /// New dimensions: (m + m') x n
-    pub fn extend_rows(&mut self, other: &Mat<T, N>)
+    pub fn extend_rows(&mut self, other: Mat<T, N>)
     where
         T: Clone,
     {
@@ -72,13 +108,13 @@ impl<T, const N: usize> Mat<T, N> {
         let (_, n2) = other.dim();
         assert_eq!(n, n2);
 
-        self.polynomials.extend(other.polynomials.clone());
+        self.polynomials.extend(other.polynomials);
     }
 
     /// Extend the matrix by adding columns.
     /// Original dimensions: m x n;
     /// New dimensions: m x (n + n')
-    pub fn extend_cols(&mut self, other: &Mat<T, N>)
+    pub fn extend_cols(&mut self, other: Mat<T, N>)
     where
         T: Clone,
     {
@@ -86,9 +122,10 @@ impl<T, const N: usize> Mat<T, N> {
         let (m2, _) = other.dim();
         assert_eq!(m, m2);
 
-        for i in 0..m {
-            self.polynomials[i].extend(other.polynomials[i].clone());
-        }
+        self.polynomials
+            .iter_mut()
+            .zip(other.polynomials)
+            .for_each(|(a, b)| a.extend(b));
     }
 }
 
@@ -173,7 +210,7 @@ mod tests {
             polynomials: vec![vec![b_0_0.clone(), b_0_1.clone()]],
         };
 
-        a.extend_rows(&b);
+        a.extend_rows(b);
 
         assert_eq!(
             a.polynomials,
@@ -202,7 +239,7 @@ mod tests {
             polynomials: vec![vec![b_0_0.clone(), b_0_1.clone()]],
         };
 
-        a.extend_cols(&b);
+        a.extend_cols(b);
 
         assert_eq!(
             a.polynomials,

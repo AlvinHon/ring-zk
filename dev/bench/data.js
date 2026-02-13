2b3a807216b3869d1e3f3856c2dbdd31bccab62f
github-action-benchmark <github@users.noreply.github.com>
2026-02-13 15:45:22 +0000

    add Rust Benchmark (customSmallerIsBetter) benchmark result for 8667ef9b9170eb622709d10da4a0b56ba8165d46

---
+++ dev/bench/data.js
@@ -0,0 +1,88 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1770997522677,
+  "repoUrl": "https://github.com/AlvinHon/ring-zk",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "AlvinHon",
+            "username": "AlvinHon"
+          },
+          "committer": {
+            "name": "AlvinHon",
+            "username": "AlvinHon"
+          },
+          "id": "8667ef9b9170eb622709d10da4a0b56ba8165d46",
+          "message": "[WIP] Add benchmark action to CI script",
+          "timestamp": "2026-02-11T14:25:03Z",
+          "url": "https://github.com/AlvinHon/ring-zk/pull/2/commits/8667ef9b9170eb622709d10da4a0b56ba8165d46"
+        },
+        "date": 1770997521721,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "linear_proof_commit",
+            "value": 14561253.39297619,
+            "unit": "ns"
+          },
+          {
+            "name": "linear_proof_create_response",
+            "value": 6777804.163683863,
+            "unit": "ns"
+          },
+          {
+            "name": "linear_proof_generate_challenge",
+            "value": 4161.604715769353,
+            "unit": "ns"
+          },
+          {
+            "name": "linear_proof_verify",
+            "value": 11758686.42330357,
+            "unit": "ns"
+          },
+          {
+            "name": "open_proof_commit",
+            "value": 5764097.399851191,
+            "unit": "ns"
+          },
+          {
+            "name": "open_proof_create_response",
+            "value": 3202623.173617725,
+            "unit": "ns"
+          },
+          {
+            "name": "open_proof_generate_challenge",
+            "value": 3179.7905470465325,
+            "unit": "ns"
+          },
+          {
+            "name": "open_proof_verify",
+            "value": 3524727.7825462967,
+            "unit": "ns"
+          },
+          {
+            "name": "sum_proof_commit",
+            "value": 35910366.54924603,
+            "unit": "ns"
+          },
+          {
+            "name": "sum_proof_commit_2",
+            "value": 9450.84117662944,
+            "unit": "ns"
+          },
+          {
+            "name": "sum_proof_create_response",
+            "value": 16654275.354420636,
+            "unit": "ns"
+          },
+          {
+            "name": "sum_proof_verify",
+            "value": 30669088.15883597,
+            "unit": "ns"
+          }
+        ]
+      }
+    ]
+  }
+}
